ff0ca3da47f05e9c12b367e60fa97c2a06daca2c
Vyacheslav Plotnikov <vyacheslavw84@gmail.com>
2026-05-11 16:34:54 +0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+type LogEntry struct {
+	Timestamp    string // время в формате "2024-01-15 10:30:00"
+	IP           string // IP адрес клиента
+	Method       string // HTTP метод (GET, POST и т.д.)
+	URL          string // путь запроса
+	StatusCode   int    // HTTP статус код
+	ResponseTime int    // время ответа в миллисекундах
+}
+
+type Statistics struct {
+	TotalRequests   int            // общее количество запросов
+	ErrorCount      int            // количество ошибок (статус >= 400)
+	RequestsByIP    map[string]int // количество запросов с каждого IP
+	AverageRespTime float64        // среднее время ответа
+}
---
+++ processor.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+)
+
+// func parseLogLine(line string) (LogEntry, error) {
+// }
+
+// func readLogs(filename string) (<-chan LogEntry, error) {
+// }
+
+// func processLogs(ctx context.Context, input <-chan LogEntry, numWorkers int) <-chan LogEntry {
+// }
+
+// func calculateStats(input <-chan LogEntry) Statistics {
+// }
